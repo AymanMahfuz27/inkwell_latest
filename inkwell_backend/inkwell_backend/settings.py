78407af63b7840d75e6e5d4428538e1a21063ab1
AymanMahfuz27 <aymanmahfuz27@utexas.edu>
2024-07-30 09:38:43 -0500

    cors

--- inkwell_backend/inkwell_backend/settings.py
+++ inkwell_backend/inkwell_backend/settings.py
@@ -149,7 +149,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:8000",
     "http://localhost:8080",
-    "https://359265b8-6edd-406f-b3a3-e869fcb2ecf8-dev.e1-us-east-azure.choreoapis.dev"
+    "https://359265b8-6edd-406f-b3a3-e869fcb2ecf8-dev.e1-us-east-azure.choreoapis.dev",
+    'https://inkwell-frontend-b355aff226c5.herokuapp.com'
 ]
 
 
