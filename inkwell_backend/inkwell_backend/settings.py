e6e48b5d763894ae6624799da38d9dd679998372
AymanMahfuz27 <aymanmahfuz27@utexas.edu>
2024-08-14 22:28:21 -0500

    again lol

--- inkwell_backend/inkwell_backend/settings.py
+++ inkwell_backend/inkwell_backend/settings.py
@@ -127,7 +127,6 @@ if os.environ.get('USE_S3') == 'True':
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     AWS_LOCATION = 'static'
-    AWS_DEFAULT_ACL = 'public-read'
     AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'us-east-1')
 
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
